9bd9c50a5e870f7383b95ee099ec3cdd7c63184d
Danthala-Abhinav <abhfirehook.ea287@gmail.com>
2021-08-20 15:56:45 +0530

    Updated

---
+++ tut02/tut02.py
@@ -0,0 +1,48 @@
+def isdigit(a):
+    flag = True
+    for i in a:
+        if(type(i)!=int):
+            flag = False
+            break
+    b=[]
+    for j in a:
+        if(type(j)!=int):
+            b.append(j)
+    if(flag == True):
+        return
+    else:
+        print('Please enter a valid input list.Invalid inputs detected : {}'.format(b))
+        exit()
+
+def get_memory_score(input_list):
+    mylist = []
+    count = 0
+    for i in input_list:
+        if(len(mylist)<5):
+            flag = False
+            for j in mylist:
+                if(i==j):
+                    count+=1
+                    flag = True
+                    break
+            if(flag == True):
+                continue
+            else:
+                mylist.append(i)
+        else:
+            flag1 = False
+            for k in mylist:
+                if(i==k):
+                    count+=1
+                    flag1 = True
+                    break
+            if(flag1 == True):
+                continue
+            else:
+                mylist.remove(mylist[0])
+                mylist.append(i)
+    return count                
+input_nums = [1,4,5.5,'s',6,7]
+isdigit(input_nums)
+score = get_memory_score(input_nums)
+print('Score: {}'.format(score))
