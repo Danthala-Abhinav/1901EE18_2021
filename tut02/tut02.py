824d2f2a3b850f7eba16cc22778fae018d07b465
Danthala-Abhinav <abhfirehook.ea287@gmail.com>
2021-08-23 17:06:43 +0530

    Added comments

--- tut02/tut02.py
+++ tut02/tut02.py
@@ -1,10 +1,15 @@
-def isdigit(a):
+# NAME    : Danthala Abhinav
+# ROLL NO : 1901EE18
+
+# MEMORY GAME
+
+def isdigit(a):                 # a represents the input list(input_list) passed 
     flag = True
     for i in a:
         if(type(i)!=int):
             flag = False
             break
-    b=[]
+    b=[]                       # b represent a list (empty when declared) where we append non-int elements
     for j in a:
         if(type(j)!=int):
             b.append(j)
@@ -14,8 +19,8 @@ def isdigit(a):
         print('Please enter a valid input list.Invalid inputs detected : {}'.format(b))
         exit()
 
-def get_memory_score(input_list):
-    mylist = []
+def get_memory_score(input_list):       # input_list represents the passed input list(input_nums)
+    mylist = []                         # mylist represents a list (empty when declared) where we append the elements as per the problem
     count = 0
     for i in input_list:
         if(len(mylist)<5):
@@ -42,7 +47,7 @@ def get_memory_score(input_list):
                 mylist.remove(mylist[0])
                 mylist.append(i)
     return count                
-input_nums = [3,4,5,3,2,1]
-isdigit(input_nums)
-score = get_memory_score(input_nums)
+input_nums = [3,4,5,3,2,1]      # Input list
+isdigit(input_nums)             # Passing input list(input_nums) to isdigit() function
+score = get_memory_score(input_nums)    # Passing input list(input_nums) to get_memory_score() function
 print('Score: {}'.format(score))
